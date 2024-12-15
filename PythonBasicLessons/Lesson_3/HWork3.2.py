f9be2c86b483f164402dfd3ed32e09f28ac15f96
Nikolay <Nikolaykov@gmail.com>
2024-12-16 00:24:27 +0200

    Homework 3.1

---
+++ PythonBasicLessons/Lesson_3/HWork3.2.py
@@ -0,0 +1,38 @@
+#The program template is substituted fo each homework example...
+###
+lst1 = [18, 42, 25, 15]
+nms1 = len(lst1)
+if nms1 != 0:
+    lst1.insert(0, lst1[-1])
+    lst1.pop()
+    print(lst1)
+else:
+    print(lst1)
+##
+lst2 = [7]
+nms2 = len(lst2)
+if nms2 != 0:
+    lst2.insert(0, lst2[-1])
+    lst2.pop()
+    print(lst2)
+else:
+    print(lst2)
+##
+lst3 = []
+nms3 = len(lst3)
+if nms3 != 0:
+    lst3.insert(0, lst3[-1])
+    lst3.pop()
+    print(lst3)
+else:
+    print(lst3)
+##
+lst4 = [7, 12, 19, 29, 33]
+nms4 = len(lst4)
+if nms4 != 0:
+    lst4.insert(0, lst4[-1])
+    lst4.pop()
+    print(lst4)
+else:
+    print(lst4)
+###
