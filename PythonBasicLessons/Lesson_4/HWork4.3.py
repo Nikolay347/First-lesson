bca19f9933c9d40fa677a3622b8e692d64666d99
Nikolay <Nikolaykov@gmail.com>
2024-12-20 00:34:00 +0200

    Homework 4.2

---
+++ PythonBasicLessons/Lesson_4/HWork4.3.py
@@ -0,0 +1,12 @@
+import random
+lst = []
+for i in range(random.randint(3, 10)):
+    lst.append(random.randint(0, 1000))
+# lst = [1, 1, 2, 1]                  # For manual testing
+print(lst)
+new_lst = [0,0,0]
+
+new_lst[0] = lst[0]
+new_lst[1] = lst[2]
+new_lst[2] = lst[-2]
+print(new_lst)
