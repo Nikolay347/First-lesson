ee2dbf9745906418097b0288aff3257e6b5df3a2
Nikolay <Nikolaykov@gmail.com>
2024-12-26 01:56:22 +0200

    Homework 5.1(draft)

---
+++ PythonBasicLessons/Lesson_5/HWork5.1.py
@@ -0,0 +1,23 @@
+
+from keyword import kwlist
+import string
+
+#rdok = input("Input variable: ")
+rdok = "_"
+var1 = 0
+for i in rdok:
+    if i in list("!”#$%&'()*+,-./:;<=>?@[\]^`{|}~"):
+        var1+=1
+
+if "__" in rdok or "___" in rdok  or " " in rdok or rdok in kwlist:
+    print(False)
+    print("Інфо по відповіді: Не відповідає умові 3 або 5.")
+elif not rdok.islower() or bool(var1) or rdok[0].isdigit():
+    print(False)
+    print("Інфо по відповіді: Не відповідає умові 2 або 4 або 6.")
+else:
+    print(True)
+#print("Умова! Змінна не повинна:\n1.Починатися з цифри.\n2.Містити великі літери.\n3.Містити пробіл, або декілька (2 або 3) нижні підкреслення, введені підряд.\n4.Містити знаки пунктуації, окрім нижнього підкреслення'_'.\n5.Бути жодним із зареєстрованих слів.\n6. Починатися з цифри.")
+
+
+
