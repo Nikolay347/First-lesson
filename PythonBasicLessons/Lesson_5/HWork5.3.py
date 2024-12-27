8cf10820fc107db5e3c075dabac22a114984a8f8
Nikolay <Nikolaykov@gmail.com>
2024-12-27 07:34:17 +0200

    Homework 5.2

---
+++ PythonBasicLessons/Lesson_5/HWork5.3.py
@@ -0,0 +1,14 @@
+import string
+ht = list((input("Input date for hashtag: ")).title())
+
+for i in ht[:]:
+    if i in string.punctuation or i == " ":
+        ht.remove(i)
+ht = ''.join(ht)
+ht1 = f"#{ht}"
+
+if len(ht1) > 140:
+    ht1 = ht1[:140]
+print("->", ht1)
+print("Hashtag initial length", len(f"#{ht}"))
+print("Hashtag result length", len(ht1))
