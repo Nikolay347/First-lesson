c9ab8987443e0a2054999531f98725d1dda5af70
Nikolay <Nikolaykov@gmail.com>
2024-12-28 23:56:06 +0200

    Homework 7.1

---
+++ PythonBasicLessons/Lesson_7/HWork7.1.py
@@ -0,0 +1,10 @@
+# Greeting
+
+def say_hi(name, age):
+    print(f"Hi! My name is {name} and I'm {age} years old!")
+
+date_1 = input("Input your name: ")
+date_2 = input("Input your age: ")
+
+say_hi(date_1, date_2)
+
