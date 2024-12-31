187e1074af0da325db26e9cc296941fc9e7b92cd
Nikolay <Nikolaykov@gmail.com>
2024-12-31 21:36:29 +0200

    Homework 7.4

---
+++ PythonBasicLessons/Lesson_7/HWork7.4.py
@@ -0,0 +1,19 @@
+# Search for common elements
+def common_elements():
+    import random
+    list_3 = []
+    # print(type(list_3))
+    list_5 = []
+    for i in range(0, 100):
+        list_3.append(int((random.randint(0, 1000))))
+        list_5.append(int((random.randint(0, 1000))))
+    set3 = {i for i in list_3}
+    set5 = {i for i in list_5}
+    # print(set3)
+    # print(set5)
+    inters = set5.intersection(set3)
+    return inters
+
+
+print("Common elements: ", common_elements())
+
