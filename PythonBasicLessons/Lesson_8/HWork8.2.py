b4f5fbeaa83d37d842e2e430cedadc303549a114
Nikolay <Nikolaykov@gmail.com>
2025-01-08 00:41:07 +0200

    Homework 8.2 Added comments

--- PythonBasicLessons/Lesson_8/HWork8.2.py
+++ PythonBasicLessons/Lesson_8/HWork8.2.py
@@ -1,17 +1,17 @@
 ## Palindrome
 import string
 def is_palindrome(text):
-    new_text = []
+    new_text = []   #clean text formation
     for i in text:
         if i in string.punctuation or  i == " ":
             continue
         else:
             i = i.lower()
             new_text.append(i)
-    new_text1 = new_text[::-1]
-    if new_text == new_text1:
+    new_text1 = new_text[::-1]      #flipping text
+    if new_text == new_text1:       #comparing texts on the subject of palindrome
         print(True)
     else:
         print(False)
 
-is_palindrome('A man, a plan, a canal: Panama')
+is_palindrome('A man, a plan, a canal: Panama')     #function calling
