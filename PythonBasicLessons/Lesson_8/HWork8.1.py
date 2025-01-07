092f1a194f99847ccbe1d20b75c8c571a1322678
Nikolay <Nikolaykov@gmail.com>
2025-01-08 00:32:34 +0200

    Homework 8.1 Сorrections after comments

--- PythonBasicLessons/Lesson_8/HWork8.1.py
+++ PythonBasicLessons/Lesson_8/HWork8.1.py
@@ -1,14 +1,14 @@
 # Add 1 to number
 
-def add_one(*args):
-    a = list(*args)
-    res = int(''.join(str(i) for i in a))
-    b = res + 1
-    k = []
+def add_one(number):
+    # a = list(number) # assigning a variable to the parameter(list)
+    numb = int(''.join(str(i) for i in list(number)))   #convert to int format
+    b = numb + 1    #adding 1 according to the condition of the task
+    k = []  #formation of the final list
     for i in str(b):
         k.append(int(i))
     print(k)
-add_one([9])
+add_one([5,6,8,7])      #function calling
 
 
 
