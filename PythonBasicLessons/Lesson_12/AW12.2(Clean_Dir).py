b7ad5a9e08485626f2f86752972951ec2441f07b
Nikolay <Nikolaykov@gmail.com>
2025-01-27 17:17:18 +0200

    Additional_work 12.2 (deleting not all files, but specified ones)

--- PythonBasicLessons/Lesson_12/AW12.2(Clean_Dir).py
+++ PythonBasicLessons/Lesson_12/AW12.2(Clean_Dir).py
@@ -1,21 +1,31 @@
-#Aditional task (AW12.2). Cleaning directory from all files.
+#Aditional task (AW12.2). Cleaning directory from some files.
 
-# The script must be situated in catalog,
-# where need remove all files!!!
-# After activation the file will be deleted together with others
-# files of this directory. Be careful! Make a copy of the catalog!..
+# The script must be situated in catalog, where need remove some files!!!
+
 import os
 
 path = os.getcwd()
 dir_list = os.listdir(path)
 print("Files in '", path, "' :")
 print(dir_list)
-choice = input("Do you want to clear the current directory together with this file? Input 'yes' or 'no': ")
-if choice == "yes":
-    for i in dir_list:
-        os.remove(i)
-    print("The directory is cleared!")
-
+choice_1 = input("Do you want to clear the current directory from some file? Input 'yes - 1' or 'no - 0': ")
+if choice_1 == "1":
+    choice_2 = int(input("Select the deletion option: 1 - at the beginning of the file name; 2 - at the ends of the file name: "))
+    if choice_2 == 1:
+        name_start = input("Enter the beginning of the file name: ")
+        print("Removal... : ")
+        for i in dir_list:
+            if i.startswith(name_start):
+                os.remove(i)
+    else:
+        name_ends = input("Enter the ends of the file: ")
+        print("Removal... : ")
+        for i in dir_list:
+            if i.endswith(name_ends):
+                os.remove(i)
+print("Verification output of left files in the directory: ")
+dir_list = os.listdir(path)
+print(dir_list)
 
 
 
